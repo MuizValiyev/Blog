e581b0e144f3f16dca9ddf1818cdfe3bff35f73f
MuizValiyev <valiyevmuiz0407@gmail.com>
2024-07-13 18:47:36 +0500

    add sitemap

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import post_detail, post_list, post_share, post_comment, add_post
+from .feeds import LatestPostsFeed
+
 
 app_name = 'main'
 
 urlpatterns = [
   path('', post_list, name='post_list'),
   path('add/', add_post, name='add'),
+  path('feed/', LatestPostsFeed(), name='post_feed'),
   path('<slug:post>/', post_detail, name='post_detail'),
   path('<int:post_id>/share/', post_share, name='post_share'),
   path('<int:post_id>/comment/', post_comment, name='post_comment'),
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from main.sitemaps import PostSitemap
+
+sitemaps = {
+'posts': PostSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap')
 ]
